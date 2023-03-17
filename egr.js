d1cc06ff7800ab40bc69340890166d4a18f35d11
nippo123 <alexcop97@mail.ru>
2023-03-18 00:38:11 +0300

    try new

--- egr.js
+++ egr.js
@@ -5,13 +5,28 @@ const fullNameInput = document.getElementById('full_name');
 unpInput.addEventListener('input', function(event) {
   const unpValue = event.target.value;
   
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(xhttp.responseText);
+    } else {
+      console.log("error")
+    }
+  };
   const url = `https://www.portal.nalog.gov.by/grp/getData?unp=${unpValue}&charset=UTF-8&type=json`;
+  xhttp.open("GET", url, true);
+  xhttp.send();
+  console.log(xhttp);
+
   
-  fetch(url)
+  
+  /*fetch(url)
     .then(response => response.json())
     .then(data => {
       nameInput.value = data.name;
       fullNameInput.value = data.full_name;
     })
-    .catch(error => console.error(error));
+    .catch(error => console.error(error));*/
 });
+
+
