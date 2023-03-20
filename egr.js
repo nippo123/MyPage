20ac0f4141f4e3382b283da027befd14475cf101
nippo123 <alexcop97@mail.ru>
2023-03-20 21:55:30 +0300

    add something

--- egr.js
+++ egr.js
@@ -2,23 +2,26 @@ const unpInput = document.getElementById('unp');
 const nameInput = document.getElementById('name');
 const fullNameInput = document.getElementById('full_name');
 
-import express from "express";
+// const express = require('express');
+import express from '/node_modules/express';
 const app = express();
 
 
 unpInput.addEventListener('input', function(event) {
   const unpValue = event.target.value;
-  
+  if (unpValue > 9) return;
   const url = `https://www.portal.nalog.gov.by/grp/getData?unp=${unpValue}&charset=UTF-8&type=json`;
   const app = express();
   app.use(cors({
-   origin:"https://www.portal.nalog.gov.by",
+   origin:url,
   }))
   fetch(url)
     .then(response => response.json())
+    
     .then(data => {
       nameInput.value = data.VNAIMP;
       fullNameInput.value = data.VNAIMK;
+      console.log(response);
     })
     .catch(error => console.error(error));
 });
